10bec74658676b6916d18c9a4a7670fa3311d683
tiankong82 <stevensha@gmail.com>
2014-10-08 22:32:39 -0700

    Add a ConfigManager.js. Also uses assert.equal() for unit tests.

--- DataCollection/Test/TestScheduler.js
+++ DataCollection/Test/TestScheduler.js
@@ -1,19 +1,18 @@
 var twitter = require('../../TwitterCrawler/twitterCrawler.js');
 var MockTwitterModule = require('../../TwitterCrawler/Test/mockTwitterModule.js');
 var Scheduler = require ('../Scheduler.js');
+var assert = require('assert');
 
 var testTweet = "testtest";
 var mockTwitterModule = new MockTwitterModule(testTweet, 1000);
 var testTwitterCrawler = new twitter.TwitterCrawler(
 	mockTwitterModule, ["DmitriCyber"], 1);
 var verify = function(content) {
-	if (content.length == 1 && content[0] == testTweet) {
-		console.log("-----PASSED------");
-	} else {
-		console.log("-----FAILED------");
-		process.exit(1);
-	}
+	assert.equal(content.length, 1, "Should only return one tweet.");
+	assert.equal(content[0], testTweet, "Tweet should be equal to " + testTweet);
+	console.log("-----PASSED------");
 }
+
 var callbacks = [];
 callbacks.push(verify);
 var scheduler = new Scheduler(testTwitterCrawler, callbacks);
