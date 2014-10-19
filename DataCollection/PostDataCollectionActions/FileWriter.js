190a65ef84f2ada7ef09ee02f77d7d129c33a357
tiankong82 <stevensha@gmail.com>
2014-10-18 21:54:52 -0700

    Update file writer to split array of objects to multiple files.

--- DataCollection/PostDataCollectionActions/FileWriter.js
+++ DataCollection/PostDataCollectionActions/FileWriter.js
@@ -1,24 +1,40 @@
 // A utility class to 1) get file name based on date/time, and
 // 2) write data to disk.
 var fs = require('fs');
-var FileWriter = function FileWriter (root) {
+var assert = require('assert');
+var FileWriter = function FileWriter (root, fileExtension) {
     this.root = root;
+    this.fileExtension = !fileExtension ? ".json" : fileExtension;
 }
 
-FileWriter.prototype.process = function (data) {
-    write(this.getFileName(this.root), data);
+FileWriter.prototype.process = function process (data) {
+    return write(this.root, this.fileExtension, data);
 }
 
-FileWriter.prototype.getFileName = function (root) {
+var getFileName = function (root, fileExtension) {
     var date = new Date();
-    return root + date.getUTCFullYear() + "_" + date.getUTCMonth() + "_" +
-    date.getUTCDate() + "_" + date.getUTCHours() + "_" + date.getUTCMinutes() + 
-    "_" + date.getUTCSeconds() + ".log";
+    return root + date.getUTCFullYear() + "_" + date.getUTCMonth() + 
+    "_" + date.getUTCDate() + "_" + date.getUTCHours() + "_" + 
+    date.getUTCMinutes() + "_" + date.getUTCMilliseconds() + "_" + 
+    Math.floor(Math.random() * 1000 + 1) + fileExtension;
 }
 
-var write = function (fileName, data) {
-    console.log("writing data to: " + fileName);
-    fs.writeFileSync(fileName, data, 'ascii');
+var write = function write (root, fileExtension, data) {
+    if (!data) {
+        return null;
+    } else if (data instanceof Array) {
+        var fileNameList = [];
+        for (var i = 0; i < data.length; ++i) {
+            fileNameList.push(write(root, fileExtension, data[i]));
+        }
+        return fileNameList;
+    } else {
+        var fileName = getFileName(root, fileExtension);
+        console.log("writing data to: " + fileName);
+        console.log("data: " + data[0] + " " + data[1] + " " + data[2]);
+        fs.writeFileSync(fileName, data, 'ascii');
+        return fileName;
+    }
 }
 
 module.exports = FileWriter;
--- DataCollection/Test/TestFileWriter.js
+++ DataCollection/Test/TestFileWriter.js
@@ -3,19 +3,47 @@ var assert = require('assert');
 var fs = require('fs');
 var root = "./";
 var writer = new FileWriter(root);
-var fileName = writer.getFileName(root);
-var testData = "test test";
-writer.process(testData);
-assert.equal(fs.existsSync(fileName), true, 
-             "filename " + fileName + " exists.");
-assert.notEqual(fileName.indexOf(".log"), -1);
-fs.readFile(fileName, 'utf8', function(err, data) {
-    // Do not expect any error.
-    assert.ifError(err);
-    assert.equal(data, testData);
-    fs.unlink(fileName, function(err) {
+
+var runTest = function (writer, testData, 
+                        expectedFileCounts,
+                        expectedOutputs) {
+    var files = writer.process(testData);
+    if (expectedOutputs instanceof Array) {
+        var mergedFiles = [];
+        mergedFiles = mergedFiles.concat.apply(mergedFiles, files);        
+        assert.equal(mergedFiles.length, 
+                     expectedFileCounts, 
+                     "lengths not equal.");
+        for(var i = 0; i < expectedFileCounts; ++i) {
+            console.log(mergedFiles[i]);
+            verify(writer, expectedOutputs[i], mergedFiles[i]);
+        }
+    } else {
+        verify(writer, expectedOutputs, files);
+    }
+}
+
+// Verifies file name pattern.
+
+var verify = function (writer, expectedContent, fileName) {
+    assert.equal(fs.existsSync(fileName), true,
+                 "file: " + fileName + " not exists.");
+    fs.readFile(fileName, 'utf8', function(err, data) {
+        // Do not expect any error.
         assert.ifError(err);
+        assert.equal(data, expectedContent, 
+                     "expected: " + expectedContent + " actual: " + data);
+        console.log("remove file: " + fileName);
+        fs.unlink(fileName, function(err) {
+            assert.ifError(err);
+        });    
     });    
-});
+}
+
+runTest(writer, "test test", 1, "test test");
+runTest(writer, ["test message 1", "test message 2"], 2, 
+        ["test message 1", "test message 2"])
+runTest(writer, [["test message 1", "test message 2"]], 2, 
+        ["test message 1", "test message 2"])
 
 console.log("-----------PASSED-------------");
