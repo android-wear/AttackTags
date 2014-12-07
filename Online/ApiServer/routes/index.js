32f2019dd282bca15b95d43bff212653f9106b88
tiankong82 <stevensha@gmail.com>
2014-12-07 00:02:02 -0800

    Implemented the homepage with 10 most recent tweets.

--- Online/ApiServer/routes/index.js
+++ Online/ApiServer/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var Tweet = require('../models/tweet');
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next){
+    var n = req.param("n");
+    if (!n) {
+        n = 10;
+    }
+    Tweet.getLatestNTweets(n, function(err, tweetsWithUrl){
+        if(err) {
+            next(err);
+        }
+        // Remove url links from text.
+        var urlRegex = /(https?:\/\/[^\s]+)/g;
+        var endsWithNonCharRegex = /\W+$/;
+        for (var i = 0; i < tweetsWithUrl.length; ++i) {
+            tweetsWithUrl[i][0] = 
+                tweetsWithUrl[i][0].replace(urlRegex, "")
+                .replace(endsWithNonCharRegex, "");   
+        }
+        res.render('index', {"n": n, "tweetsWithUrl": tweetsWithUrl});
+    });
 });
-
 module.exports = router;
