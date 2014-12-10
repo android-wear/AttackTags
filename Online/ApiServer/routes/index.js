2ed67c96552f423c99cc7e80602a189ee3e76ffa
tiankong82 <stevensha@gmail.com>
2014-12-09 23:45:09 -0800

    Remove 1) duplicated entries, and 2) empty entries.

--- Online/ApiServer/routes/index.js
+++ Online/ApiServer/routes/index.js
@@ -23,12 +23,22 @@ router.get('/', function(req, res, next){
         // before: asdfasdf @yangsha @anotherguy qqqq.
         // after: both @yangsha and @anogherguy removed.
         var pingOtherPeoplePatternRegex = /@\w+/g;
-        for (var i = 0; i < tweetsWithUrl.length; ++i) {
+        var existedUrls = [];
+        var existedTitles = [];
+        for (var i = 0; i < tweetsWithUrl.length; ++i) {            
             tweetsWithUrl[i][0] = 
                 tweetsWithUrl[i][0].replace(urlRegex, "")
                 .replace(endsWithNonCharRegex, "")
                 .replace(reTweetPatternRegex, "")
                 .replace(pingOtherPeoplePatternRegex, "");
+            if (!tweetsWithUrl[i][0] || existedTitles.indexOf(tweetsWithUrl[i][0]) >= 0 ||
+                    existedUrls.indexOf(tweetsWithUrl[i][1]) >= 0) {
+                tweetsWithUrl.splice(i, 1);
+                --i;
+            } else {
+                existedTitles.push(tweetsWithUrl[i][0]);
+                existedUrls.push(tweetsWithUrl[i][1]);
+            }
         }
         res.render('index', {"n": n, "tweetsWithUrl": tweetsWithUrl});
     });
