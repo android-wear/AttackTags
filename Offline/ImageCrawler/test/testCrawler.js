efa59ffb0feeb8f452c12962e577e11f0a6827c7
tiankong82 <stevensha@gmail.com>
2014-12-19 22:40:52 -0800

    Fix crawler.

--- Offline/ImageCrawler/test/testCrawler.js
+++ Offline/ImageCrawler/test/testCrawler.js
@@ -1,9 +1,2 @@
-/*
 var Crawler = require('../crawler.js');
 Crawler.crawl(456, "hello world", console.log);
-*/
-
-var testDate = new Date('Tue Dec 09 03:50:04 +0000 2014');
-console.log(testDate.getTime());
-var anotherDate = new Date(1418097004000);
-console.log(testDate.getUTCHours());
