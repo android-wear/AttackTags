38dd6c5d01b5c0f96a9d97860937149dc361921e
Empti <emptitest@gmail.com>
2015-04-21 23:48:20 -0700

    modifying utils kill and start process files, adding support for Runner.js

--- utils/start_processes.sh
+++ utils/start_processes.sh
@@ -1,3 +1,7 @@
+echo Start Crawler
+cd ~/Offline/DataCollection
+sudo NODE_ENV=production forever start -w Runner.js
+
 echo Start Processor.
 cd ~/Offline/DataProcessing
 sudo NODE_ENV=production forever start -w DataImporter.js
@@ -12,4 +16,4 @@ sudo NODE_ENV=production forever start -w tweetProcessor.js
 
 # Check process
 echo "Current running processes:"
-ps aux | grep -E 'DataImporter|hashtagProcessor|tweetProcessor' | grep forever | awk '{print $2, $13;}'
+ps aux | grep -E 'Runner|DataImporter|hashtagProcessor|tweetProcessor' | grep forever | awk '{print $2, $13;}'
